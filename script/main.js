e357dd90a1fd3a90aadb9cb29f4d43058b4b0190
RodrigoH95 <HRodrigo95@hotmail.com>
2022-04-10 04:28:18 -0300

    Menu de navegación responsivo tipo hamburguesa

---
+++ script/main.js
@@ -0,0 +1,12 @@
+const HAMBURGER = document.querySelector(".hamburguer");
+const NAV_BAR = document.querySelector(".nav-bar");
+
+HAMBURGER.addEventListener("click", () => {
+    HAMBURGER.classList.toggle("active");
+    NAV_BAR.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-enlace").forEach(n => n.addEventListener("click", () =>{
+    HAMBURGER.classList.remove("active");
+    NAV_BAR.classList.remove("active");
+}))
