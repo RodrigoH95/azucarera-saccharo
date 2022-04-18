d8684ae777b1d6f7aa9137ca179c9f2fd8b1a1dd
RodrigoH95 <HRodrigo95@hotmail.com>
2022-04-18 00:44:52 -0300

    Se agregó menú desplegable en seccion procesos para mejor navegación en dispositivos medianos/pequeños

--- script/main.js
+++ script/main.js
@@ -1,12 +1,18 @@
 const HAMBURGER = document.querySelector(".hamburguer");
 const NAV_BAR = document.querySelector(".nav-bar");
+const MENU_DESPLEGABLE = document.querySelector(".menu-desplegable");
+const INDICE = document.querySelector(".indice");
 
-HAMBURGER.addEventListener("click", () => {
-    HAMBURGER.classList.toggle("active");
-    NAV_BAR.classList.toggle("active");
+HAMBURGER.addEventListener("click", () => NAV_BAR.classList.toggle("active"))
+
+document.querySelectorAll(".nav-enlace").forEach(n => n.addEventListener("click", () => NAV_BAR.classList.remove("active")));
+
+MENU_DESPLEGABLE.addEventListener("click", () => {
+    MENU_DESPLEGABLE.classList.toggle("active");
+    INDICE.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-enlace").forEach(n => n.addEventListener("click", () =>{
-    HAMBURGER.classList.remove("active");
-    NAV_BAR.classList.remove("active");
+document.querySelectorAll(".indice-link").forEach(n => n.addEventListener("click", () =>{
+    MENU_DESPLEGABLE.classList.remove("active");
+    INDICE.classList.remove("active");
 }));
