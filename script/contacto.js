23371b33843c44735d06096142a5ed6b642f70ee
RodrigoH95 <HRodrigo95@hotmail.com>
2022-05-28 20:24:46 -0300

    Se modificó el menu desplegable de la sección producción para que se adapte a dispositivos móviles si estos están en modo apaisado (horizontal)

--- script/contacto.js
+++ script/contacto.js
@@ -1,17 +1,20 @@
+// Se obtiene el elemento form del html
 const form = document.getElementById("contacto");
 
-
-
 form.addEventListener("submit", (e) => {
+
+  // Se obtienen todos los valores de los inputs que interesan validar
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const email = document.getElementById("correo").value;
   const localidad = document.getElementById("localidad").value;
   const asunto = document.getElementById("asunto").value;
   const comentario = document.getElementById("comentario").value;
-
+  
+  // Elemento div vacío con id "error" donde se cargarán los mensajes
   const error = document.getElementById("error");
-
+  
+  // Lista de mensajes que inicia vacía y se va llenando a medida que se encuentra un error
   const MENSAJES = [];
   error.innerText = "";
   if (!isValid(nombre)) {
@@ -46,13 +49,15 @@ form.addEventListener("submit", (e) => {
     MENSAJES.push("*Su mensaje debe contener al menos 10 caracteres");
   }
 
+  // Si la lista de mensajes tiene al menos un elemento, se ejecuta lo siguiente, caso contrario, se envía el formulario
   if (MENSAJES.length > 0) {
     e.preventDefault();
+    // Se cargan todos los errores de la lista de mensajes en el elemento div unidos por "\n" que es un quiebre de linea
     error.innerText = MENSAJES.join("\n");
   }
 });
 
-
+// Esta funcion comprueba que los inputs no se envíen en blanco/nulos
 function isValid(element) {
     return element !== '' && element !== null;
 }
